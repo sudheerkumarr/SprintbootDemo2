8151ba90c4d6383ca1e6ad084c0d30814994780b
rskumar780@gmail.com <rskumar780@gmail.com>
2023-11-20 12:53:50 +0530

    login added

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -7,12 +7,25 @@ public class LoginController {
 	
 	// login
 	/*
-	 * email, password, role - input
-	 * email, role, isLogin
+	 * email, password, role - input - inputDto
+	 * compare email,password & role info with db details 
+	 * if details are matching return
+	 *   set isLogin flag as true 
+	 *   email, role, isLogin - response - outputDto
+	 * else 
+	 *   throw exception InvalidCredentials
 	 */
 	
 	
 	
 	// logout
+	/*    email - input 
+	 *  set isLogin flag as false
+	 *    return - email, isLogin, role - outputDto
+	 * 
+	 */
 
+	
+	// Reset
+	
 }
