0d0f46d352c875ac83afe9022ee7224082774a56
rskumar780@gmail.com <rskumar780@gmail.com>
2023-12-07 10:30:43 +0530

    Added login/logout functionality

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.example.exception.InvalidCredentialsException;
 import com.example.service.LoginService;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 	
 	@Autowired
